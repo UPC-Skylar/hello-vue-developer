2ed3a7397b4558414a58add2acf8c69a151e8dfe
UPC-Skylar <zaynml2021@gmail.com>
2025-05-22 16:25:05 -0500

    feat(greet-developer): add greet developer component.

---
+++ src/greetings/components/greet-developer.component.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "greet-developer",
+  props: {
+    firstName: String,
+    lastName: String,
+  },
+  methods: {
+    getGreeting(){
+      if (!this.firstName?.trim() || !this.lastName?.trim()){
+        return "Welcome Anonymous Developer";
+      }
+      const developer = new Developer(this.firstName, this.lastName);
+      return `Congrats ${developer.fullName}! Now you are a Vue Developer`;
+    }
+  }
+}
+</script>
+  <p>{{ getGreeting() }}</p>
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
